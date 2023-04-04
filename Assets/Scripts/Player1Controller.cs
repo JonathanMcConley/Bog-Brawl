9fee9470ae54a840959f45bb3f0827b7af372d4b
JonathanMcConley <73368065+JonathanMcConley@users.noreply.github.com>
2023-04-03 22:37:54 -0400

    Lots of Changes. Please help with setting up nodes again.

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Player1Controller : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
+    private Animator animatorController;
+    public Transform punchAttackPoint;
+    public Transform kickAttackPoint;
+    public float punchAttackRange;
+    public float kickAttackRange;
     private bool isSinking;
     private float jumpHeight;
     private float speed;
@@ -14,15 +19,14 @@ public class Player1Controller : MonoBehaviour
     private float yDeathboxMin;
     private float yDeathboxMax;
     private float xDeathboxMin;
-    public bool isPunching;
-    public bool isKicking;
-    public bool isJumping;
-    public bool isWalking;
-    public bool gotHit;
+    private float punchTime;
+    private float kickTime;
+    public float hitRecoveryTime;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animatorController = GetComponent<Animator>();
         isSinking = false;
         jumps = 2;
         jumpResistance = 1;
@@ -32,6 +36,8 @@ public class Player1Controller : MonoBehaviour
         speed = 10;
         jumpHeight = 10;
         minSinkingVelocity = -0.1f;
+        punchTime = 0.01f;
+        kickTime = 0.3325f;
     }
 
     // Update is called once per frame
@@ -51,35 +57,59 @@ public class Player1Controller : MonoBehaviour
             {
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, minSinkingVelocity);
             }
+            animatorController.SetBool("IsJumping", false);
         }
         if (!isSinking)
         {
             jumpResistance = 1;
+            animatorController.SetBool("IsJumping", true);
         }
         //Jump with X for P1, and Numpad 8 for P2
-        if (Input.GetKeyDown(KeyCode.X) && jumps > 0)
+        if (Input.GetKeyDown(KeyCode.X) && jumps > 0 && !animatorController.GetBool("GotHit"))
         {
             rigidBody.AddForce(Vector2.up * jumpHeight * jumpResistance, ForceMode2D.Impulse);
             jumps -= 1;
         }
         //Light Attack with Spacebar for P1, and Numpad 4 for P2
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && !animatorController.GetBool("GotHit")) 
         {
-            
+            StartCoroutine("PunchCoroutine");
         }
         //Heavy Attack with V for P1, and Numpad 5 for P2
-        if (Input.GetKeyDown(KeyCode.V)) 
+        if (Input.GetKeyDown(KeyCode.V) && !animatorController.GetBool("GotHit")) 
         {
-        
+            StartCoroutine("KickCoroutine");
         }
         //Move Left/ Right with A and D for P1, and Left and Right Arrowkeys for P2
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !animatorController.GetBool("GotHit"))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
+            if (isSinking)
+            {
+                animatorController.SetBool("IsWalking", true);
+            }
+            else 
+            {
+                animatorController.SetBool("IsWalking", false);
+            }
+            gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) && !animatorController.GetBool("GotHit"))
+        {
+            if (isSinking)
+            {
+                animatorController.SetBool("IsWalking", true);
+            }
+            else
+            {
+                animatorController.SetBool("IsWalking", false);
+            }
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)) 
+        {
+            animatorController.SetBool("IsWalking", false);
         }
     }
     //Allows Player to jump while they're touching ground, reduces gravity while touching ground
@@ -100,6 +130,46 @@ public class Player1Controller : MonoBehaviour
     }
 
     //Coroutine for Punching
+    private IEnumerator PunchCoroutine()
+    {
+        animatorController.SetBool("IsPunching", true);
+        //Collision detection by Brackeys
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(punchAttackPoint.position, punchAttackRange);
+        foreach (Collider2D hitPlayer in hitPlayers) 
+        {
+            if (hitPlayer.CompareTag("Player2")) 
+            {
+                //Do damage
+                //Assign knockback based on damage
+                //Call their RecoveryFromHitCoroutine
+            }
+        }
+        yield return new WaitForSeconds(punchTime);
+        animatorController.SetBool("IsPunching", false);
+    }
     //Coroutine for Kicking
+    private IEnumerator KickCoroutine() 
+    {
+        animatorController.SetBool("IsKicking", true);
+        //Collision detection by Brackeys
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(punchAttackPoint.position, punchAttackRange);
+        foreach (Collider2D hitPlayer in hitPlayers)
+        {
+            if (hitPlayer.CompareTag("Player2"))
+            {
+                //Do damage
+                //Assign knockback based on damage
+                //Call their RecoveryFromHitCoroutine
+            }
+        }
+        yield return new WaitForSeconds(kickTime);
+        animatorController.SetBool("IsKicking", false);
+    }
     //Coroutine for Getting Hit
+    public IEnumerator RecoveryFromHitCoroutine() 
+    {
+        animatorController.SetBool("GotHit", true);
+        yield return new WaitForSeconds(hitRecoveryTime);
+        animatorController.SetBool("GotHit", false);
+    }
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -5,26 +5,39 @@ using UnityEngine;
 public class Player2Controller : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
-    public bool isSinking;
-    public float jumpHeight;
-    public float speed;
-    private float horizontalInput;
-    public float minSinkingVelocity;
-    public int jumps;
+    private Animator animatorController;
+    public Transform punchAttackPoint;
+    public Transform kickAttackPoint;
+    public float punchAttackRange;
+    public float kickAttackRange;
+    private bool isSinking;
+    private float jumpHeight;
+    private float speed;
+    private float minSinkingVelocity;
+    private int jumps;
     private float jumpResistance;
     private float yDeathboxMin;
     private float yDeathboxMax;
     private float xDeathboxMin;
+    private float punchTime;
+    private float kickTime;
+    public float hitRecoveryTime;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        animatorController = GetComponent<Animator>();
         isSinking = false;
         jumps = 2;
         jumpResistance = 1;
         yDeathboxMin = -15;
         xDeathboxMin = -20;
         yDeathboxMax = 30;
+        speed = 10;
+        jumpHeight = 10;
+        minSinkingVelocity = -0.1f;
+        punchTime = 0.01f;
+        kickTime = 0.3325f;
     }
 
     // Update is called once per frame
@@ -36,7 +49,7 @@ public class Player2Controller : MonoBehaviour
             rigidBody.velocity = Vector2.zero;
             gameObject.transform.position = new Vector3(0, 0, -0.01f);
         }
-        //If the player is sinking, their gravity is 0.001% of Normal Gravity and their jump is less effective
+        //If the player is sinking, their gravity is 0.001% of Normal Gravity
         if (isSinking)
         {
             jumpResistance = 0.625f;
@@ -44,34 +57,59 @@ public class Player2Controller : MonoBehaviour
             {
                 rigidBody.velocity = new Vector2(rigidBody.velocity.x, minSinkingVelocity);
             }
+            animatorController.SetBool("IsJumping", false);
         }
         if (!isSinking)
         {
             jumpResistance = 1;
+            animatorController.SetBool("IsJumping", true);
         }
         //Jump with X for P1, and Numpad 8 for P2
-        if (Input.GetKeyDown(KeyCode.Keypad8) && jumps > 0)
+        if (Input.GetKeyDown(KeyCode.Keypad8) && jumps > 0 && !animatorController.GetBool("GotHit"))
         {
             rigidBody.AddForce(Vector2.up * jumpHeight * jumpResistance, ForceMode2D.Impulse);
             jumps -= 1;
         }
         //Light Attack with Spacebar for P1, and Numpad 4 for P2
-        if (Input.GetKeyDown(KeyCode.Keypad4)) 
+        if (Input.GetKeyDown(KeyCode.Keypad4) && !animatorController.GetBool("GotHit"))
         {
-        
+            StartCoroutine("PunchCoroutine");
         }
         //Heavy Attack with V for P1, and Numpad 5 for P2
-        if (Input.GetKeyDown(KeyCode.Keypad5)) 
+        if (Input.GetKeyDown(KeyCode.Keypad5) && !animatorController.GetBool("GotHit"))
         {
-        
+            StartCoroutine("KickCoroutine");
         }
         //Move Left/ Right with A and D for P1, and Left and Right Arrowkeys for P2
-        if (Input.GetKey(KeyCode.LeftArrow)) {
+        if (Input.GetKey(KeyCode.LeftArrow) && !animatorController.GetBool("GotHit"))
+        {
+            if (isSinking)
+            {
+                animatorController.SetBool("IsWalking", true);
+            }
+            else
+            {
+                animatorController.SetBool("IsWalking", false);
+            }
+            gameObject.transform.rotation = new Quaternion(0, 0, 0, 0);
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) && !animatorController.GetBool("GotHit"))
         {
-            transform.Translate(Vector3.right* speed * Time.deltaTime);
+            if (isSinking)
+            {
+                animatorController.SetBool("IsWalking", true);
+            }
+            else
+            {
+                animatorController.SetBool("IsWalking", false);
+            }
+            gameObject.transform.rotation = new Quaternion(0, 180, 0, 0);
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
+            animatorController.SetBool("IsWalking", false);
         }
     }
     //Allows Player to jump while they're touching ground, reduces gravity while touching ground
@@ -92,6 +130,46 @@ public class Player2Controller : MonoBehaviour
     }
 
     //Coroutine for Punching
+    private IEnumerator PunchCoroutine()
+    {
+        animatorController.SetBool("IsPunching", true);
+        //Collision detection by Brackeys
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(punchAttackPoint.position, punchAttackRange);
+        foreach (Collider2D hitPlayer in hitPlayers)
+        {
+            if (hitPlayer.CompareTag("Player1"))
+            {
+                //Do damage
+                //Assign knockback based on damage
+                //Call their RecoveryFromHitCoroutine
+            }
+        }
+        yield return new WaitForSeconds(punchTime);
+        animatorController.SetBool("IsPunching", false);
+    }
     //Coroutine for Kicking
+    private IEnumerator KickCoroutine()
+    {
+        animatorController.SetBool("IsKicking", true);
+        //Collision detection by Brackeys
+        Collider2D[] hitPlayers = Physics2D.OverlapCircleAll(punchAttackPoint.position, punchAttackRange);
+        foreach (Collider2D hitPlayer in hitPlayers)
+        {
+            if (hitPlayer.CompareTag("Player1"))
+            {
+                //Do damage
+                //Assign knockback based on damage
+                //Call their RecoveryFromHitCoroutine
+            }
+        }
+        yield return new WaitForSeconds(kickTime);
+        animatorController.SetBool("IsKicking", false);
+    }
     //Coroutine for Getting Hit
+    public IEnumerator RecoveryFromHitCoroutine()
+    {
+        animatorController.SetBool("GotHit", true);
+        yield return new WaitForSeconds(hitRecoveryTime);
+        animatorController.SetBool("GotHit", false);
+    }
 }
